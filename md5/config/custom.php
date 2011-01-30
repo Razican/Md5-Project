d9a0ebfb8aae578e9620fb702c8d108590610bbb
Razican <devnull@localhost>
2011-01-30 01:00:42 +0100

    Se cambian pequeños detalles.

--- md5/config/custom.php
+++ md5/config/custom.php
@@ -6,12 +6,12 @@ if (defined('IN_ADMIN'))
 }
 
 $config['skin']				= "Md52";
-$config['version']			= '2 PA3';
+$config['version']			= '2.0pa3';
 
 $config['cookie_domain']	= "testing.md5-project.razican.com";
 
 $config['admin_username']	= "Admin";
 $config['admin_password']	= "8cb2237d0679ca88db6464eac60da96345513964";
 
-/* End of file extra.php */
-/* Location: ./system/application/config/extra.php */
+/* End of file custom.php */
+/* Location: ./system/application/config/custom.php */
--- system/language/spanish/form_validation_lang.php
+++ system/language/spanish/form_validation_lang.php
@@ -2,22 +2,22 @@
 
 $lang['required'] 			= "El campo %s es obligatorio.";
 $lang['isset']				= "El campo %s debe contener un valor.";
-$lang['valid_email']		= "El campo %s debe contener una dirección de email válida.";
-$lang['valid_emails'] 		= "El campo %s debe contener todas las direcciones de email válidas.";
-$lang['valid_url'] 			= "El campo %s debe contener una URL válida.";
-$lang['valid_ip'] 			= "El campo %s debe contener una IP válida.";
+$lang['valid_email']		= "El campo %s debe contener una direcciÃ³n de email vÃ¡lida.";
+$lang['valid_emails'] 		= "El campo %s debe contener todas las direcciones de email vÃ¡lidas.";
+$lang['valid_url'] 			= "El campo %s debe contener una URL vÃ¡lida.";
+$lang['valid_ip'] 			= "El campo %s debe contener una IP vÃ¡lida.";
 $lang['min_length']			= "El campo %s debe tener al menos %s caracteres de longitud.";
 $lang['max_length']			= "El campo %s no puede exceder los %s caracteres de longitud.";
 $lang['exact_length']		= "El campo %s debe tener exactamente %s caracteres de longitud.";
-$lang['alpha']				= "El campo %s debe contener sólo caracteres alfabéticos.";
-$lang['alpha_numeric']		= "El campo %s debe contener sólo caracteres alfa-numéricos.";
-$lang['alpha_dash']			= "El campo %s debe contener sólo caracteres alfa-numéricos, guiones bajos y guiones.";
-$lang['numeric']			= "El campo %s debe contener sólo números.";
-$lang['is_numeric']			= "El campo %s debe contener sólo caracteres numéricos.";
-$lang['integer']			= "El campo %s debe contener un número entero.";
+$lang['alpha']				= "El campo %s debe contener solo caracteres alfabÃ©ticos.";
+$lang['alpha_numeric']		= "El campo %s debe contener solo caracteres alfa-numÃ©ricos.";
+$lang['alpha_dash']			= "El campo %s debe contener solo caracteres alfa-numÃ©ricos, guiones bajos y guiones.";
+$lang['numeric']			= "El campo %s debe contener solo nÃºmeros.";
+$lang['is_numeric']			= "El campo %s debe contener solo caracteres numÃ©ricos.";
+$lang['integer']			= "El campo %s debe contener un nÃºmero entero.";
 $lang['matches']			= "El campo %s no coincide con el campo %s.";
-$lang['is_natural']			= "El campo %s debe contener sólo múmeros positivos.";
-$lang['is_natural_no_zero']	= "El campo %s debe contener un número mayor que cero.";
+$lang['is_natural']			= "El campo %s debe contener sÃ³lo mÃºmeros positivos.";
+$lang['is_natural_no_zero']	= "El campo %s debe contener un nÃºmero mayor que cero.";
 
 
 /* End of file form_validation_lang.php */
