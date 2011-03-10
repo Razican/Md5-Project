7976f868b1b8db87e463b8e5fc23f387ab0ea384
Razican <devnull@localhost>
2011-03-10 23:17:49 +0100

    Se mejora el skin, pero todavía sin acabar.

--- md5/views/Md52/home.php
+++ md5/views/Md52/home.php
@@ -2,6 +2,18 @@
 
 <?php echo $menu ?>
 
+		<div class="content">
+			<div class="content-logo"><?php echo img(array('src'	=> base_url().'styles/images/logo.png', 'alt'	=> 'Logo')); ?></div>
+			<div class="paypal-form">
+				<?php echo form_open('https://www.paypal.com/cgi-bin/webscr', '', $hidden); ?>
+					<?php echo paypal_input(); ?>
+				<?php echo form_close() ?>
+			</div>
+			<div class="content-desc"><?php echo lang('home.description'); ?>:</div>
+			<div class="home-button"><input type="image" src="<?php echo skin_path(); ?>images/<?php echo $this->lang->lang(); ?>/encryptor.png" alt="<?php echo lang('home.encryptor'); ?>" onClick="top.location.href='<?php echo $this->lang->lang(); ?>/encryptor'" ></div>
+			<div class="home-button"><input type="image" src="<?php echo skin_path(); ?>images/<?php echo $this->lang->lang(); ?>/encryptor.png" alt="<?php echo lang('home.encryptor'); ?>" onClick="top.location.href='<?php echo $this->lang->lang(); ?>/encryptor'" ></div>
+		</div>
+
 		<table width="548" border="0" height="413"  background="<?php echo skin_path(); ?>images/indextable.png">
 			<tbody>
 			<tr>
