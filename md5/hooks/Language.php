d0e921d44a8ea9985102cba0cd53cc5e6c49b49c
Iban Eguia <admin@razican.com>
2011-08-22 17:24:27 +0200

    Se crea una detección del lenguaje del usuario.
    No es muy efectiva, ya que usa la prioridad de los lenguajes en la lubrería de lenguaje y no la del user_agent.

--- md5/hooks/Language.php
+++ md5/hooks/Language.php
@@ -17,15 +17,26 @@ function language()
 	$language	= $CI->session->userdata('language');
 	if(($language != $CI->lang->lang()) && ($CI->lang->lang() != ''))
 	{
-	$CI->session->set_userdata('language', $CI->lang->lang());
+		$CI->session->set_userdata('language', $CI->lang->lang());
 	}
-	else if(($CI->lang->lang() == '') && ($language == ''))
+	else if(( ! $CI->lang->lang()) && ( ! $language))
 	{
+		$CI->load->library('user_agent');
+		foreach ($CI->lang->languages as $lang => $name)
+		{
+			if($CI->agent->accept_lang($lang))
+			{
+				$CI->session->set_userdata('language', $lang);
+				log_message('debug', 'Redirect to '.$lang.$CI->uri->uri_string());
+				redirect($lang.$CI->uri->uri_string(), 'location', 302);
+				return;
+			}
+		}
 		$CI->session->set_userdata('language', $CI->lang->default_lang());
 		log_message('debug', 'Redirect to '.$CI->lang->default_lang().$CI->uri->uri_string());
 		redirect($CI->lang->default_lang().$CI->uri->uri_string(), 'location', 302);
 	}
-	else if ($CI->lang->lang() == '')
+	else if ( ! $CI->lang->lang())
 	{
 		log_message('debug', 'Redirect to '.$language.'/'.$CI->uri->uri_string());
 		redirect($language.'/'.$CI->uri->uri_string(), 'location', 302);
